e7dcfe292dcf5d939cabb5dad55741b1354d70b2
ngzHappy <zhaixueqiang@hotmail.com>
2018-12-04 14:05:18 +0800

    +change this file to help build the project by msvc

---
+++ build_msvc.bat
@@ -0,0 +1,17 @@
+call "C:/Qt/Qt5.12.0/5.12.0/msvc2017_64/bin/qtenv2.bat"
+cd /D "E:/QtQmlBookMsvc"
+qmake -r -tp vc "E:/QtQmlBook/QtQmlBook.pro"
+qmake -r -tp vc "E:/QtQmlBook/QtQmlBookTest.pro"
+cmd
+
+
+
+
+
+
+
+
+
+
+
+
