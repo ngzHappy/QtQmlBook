5fd3aa32f297b1d7e2483c55317eef9cec7d5077
ngzHappy <zhaixueqiang@hotmail.com>
2018-12-04 16:54:35 +0800

    +test

--- sstd_qt_and_qml_library/test/main.cpp
+++ sstd_qt_and_qml_library/test/main.cpp
@@ -1,13 +1,13 @@
 ﻿#include <sstd_qt_and_qml_library.hpp>
 #include "TestObject.hpp"
 
-int main(int argc,char ** argv){
+int main(int argc, char ** argv) {
 
-    auto varApp = sstd_make_unique<sstd::Application>(argc,argv);
+    auto varApp = sstd_make_unique<sstd::Application>(argc, argv);
 
-    TestObject varTestObject;
+    auto varTestObject = sstd_make_shared< TestObject >();
 
-    return QTest::qExec(&varTestObject, argc, argv);
+    return QTest::qExec(varTestObject.get(), argc, argv);
 
 }
 
