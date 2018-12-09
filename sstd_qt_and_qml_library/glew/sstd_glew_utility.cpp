1630a710b83447b60419e2064e5ec0445f98c3d8
ngzHappy <zhaixueqiang@hotmail.com>
2018-12-09 19:18:58 +0800

    +test code

---
+++ sstd_qt_and_qml_library/glew/sstd_glew_utility.cpp
@@ -0,0 +1,42 @@
+﻿#define _SSTD_HAS_GLEW_0_1
+
+#include "GLSSTD/glew.h"
+#include "sstd_glew_utility.hpp"
+
+namespace sstd {
+
+
+    EXPORT_SSTD_QT_AND_QML_LIBRARY GLuint createTexture(QImage arg){
+
+        const auto varImage =
+                arg.convertToFormat( QImage::Format_RGBA64 );
+
+        if( varImage.isNull() ){
+            return 0;
+        }
+
+        //glCreateTextures();
+
+
+
+        return 0;
+    }
+
+    EXPORT_SSTD_QT_AND_QML_LIBRARY void updateTexture(GLuint,QImage){}
+
+}/*namespace sstd*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
