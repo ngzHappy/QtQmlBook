7f82358d634acab8bc05275d1c3d9d3b70272cf9
ngzHappy <zhaixueqiang@hotmail.com>
2018-12-09 20:04:38 +0800

    +test code

--- sstd_qt_and_qml_library/glew/sstd_glew_utility.cpp
+++ sstd_qt_and_qml_library/glew/sstd_glew_utility.cpp
@@ -1,28 +1,77 @@
-﻿#define _SSTD_HAS_GLEW_0_1
+﻿#define _SSTD_HAS_GLEW_0_1 (1u)
 
 #include "GLSSTD/glew.h"
 #include "sstd_glew_utility.hpp"
+#include <cassert>
 
-namespace sstd {
+namespace sstd::opengl_utility {
 
+    EXPORT_SSTD_QT_AND_QML_LIBRARY QSize getTextureSize(GLuint arg) {
 
-    EXPORT_SSTD_QT_AND_QML_LIBRARY GLuint createTexture(QImage arg){
+        if (arg < 1) {
+            return{ 0,0 };
+        }
+
+        GLint width{ 0 };
+        GLint height{ 0 };
+        glGetTextureLevelParameteriv(arg, 0, GL_TEXTURE_WIDTH, &width);
+        glGetTextureLevelParameteriv(arg, 0, GL_TEXTURE_HEIGHT, &height);
+
+        return{ width,height };
+
+    }
+
+    EXPORT_SSTD_QT_AND_QML_LIBRARY GLuint createTexture(QImage arg) {
 
         const auto varImage =
-                arg.convertToFormat( QImage::Format_RGBA64 );
+            arg.convertToFormat(QImage::Format_RGBA64);
 
-        if( varImage.isNull() ){
+        if (varImage.isNull()) {
             return 0;
         }
 
-        //glCreateTextures();
+        GLuint varAns{ 0 };
+        glCreateTextures(GL_TEXTURE_2D, 1, &varAns);
 
+        if (varAns < 1) {
+            return 0;
+        }
 
-
-        return 0;
+        glTextureStorage2D(varAns, 1, GL_RGBA16UI, 
+            varImage.width(), varImage.height());
+        glTextureSubImage2D(varAns, 0,
+            0, 0,
+            varImage.width(), varImage.height(),
+            GL_RGBA, GL_UNSIGNED_SHORT,
+            varImage.constBits());
+        return varAns;
     }
 
-    EXPORT_SSTD_QT_AND_QML_LIBRARY void updateTexture(GLuint,QImage){}
+    EXPORT_SSTD_QT_AND_QML_LIBRARY void updateTexture(GLuint * argTextureID, QImage argImage) {
+
+        assert(argTextureID);
+
+        if (*argTextureID < 1) {
+            *argTextureID = createTexture(argImage);
+            return;
+        }
+
+        auto varImageSize = argImage.size();
+        auto varTextureSize = getTextureSize(*argTextureID);
+
+        if (varImageSize != varTextureSize) {
+            *argTextureID = createTexture(argImage);
+            return;
+        }
+
+        const auto & varImage = argImage;
+        glTextureSubImage2D(*argTextureID, 0,
+            0, 0,
+            varImage.width(), varImage.height(),
+            GL_RGBA, GL_UNSIGNED_SHORT,
+            varImage.constBits());
+
+    }
 
 }/*namespace sstd*/
 
@@ -30,7 +79,9 @@ namespace sstd {
 
 
 
-
+//
+// https://www.khronos.org/registry/OpenGL-Refpages/gl4/
+//
 
 
 
--- sstd_qt_and_qml_library/glew/sstd_glew_utility.hpp
+++ sstd_qt_and_qml_library/glew/sstd_glew_utility.hpp
@@ -2,16 +2,17 @@
 
 #include "../global/sstd_qt_and_qml_global.hpp"
 #include <QtGui/qimage.h>
+
 #ifndef _SSTD_HAS_GLEW_0_1
 #include <QtGui/qopenglfunctions.h>
 #include <QtGui/qopenglfunctions_4_5_core.h>
 #endif
 
-namespace sstd{
+namespace sstd::opengl_utility {
 
-    EXPORT_SSTD_QT_AND_QML_LIBRARY GLuint createTexture(QImage );
-    EXPORT_SSTD_QT_AND_QML_LIBRARY void updateTexture(GLuint,QImage);
-
+    EXPORT_SSTD_QT_AND_QML_LIBRARY GLuint createTexture(QImage);
+    EXPORT_SSTD_QT_AND_QML_LIBRARY QSize getTextureSize(GLuint);
+    EXPORT_SSTD_QT_AND_QML_LIBRARY void updateTexture(GLuint *, QImage);
 
 }/*namespace sstd*/
 
