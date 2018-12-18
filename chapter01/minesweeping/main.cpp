8f8edfbacf3b76dab2eaa76d9fcd5e77a3e57a03
ngzHappy <zhaixueqiang@hotmail.com>
2018-12-18 18:25:32 +0800

    +

--- chapter01/minesweeping/main.cpp
+++ chapter01/minesweeping/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char ** argv) {
     auto varApp = sstd_make_unique< sstd::Application >(argc, argv);
 
     /*初始化Qml/Quick引擎*/
-    auto varWindow = sstd_new< sstd::DefaultRoowWindow >();
+    auto varWindow = sstd_make_unique< sstd::DefaultRoowWindow >();
     {
         /*获得Qml文件绝对路径*/
         auto varFullFileName = sstd::getLocalFileFullPath(
