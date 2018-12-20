ed9dd3264097eb422e4c4702c755ae7fae6e8022
ngzHappy <zhaixueqiang@hotmail.com>
2018-12-20 11:01:51 +0800

    +linux tcmalloc

--- sstd_library/_sstd_library_memory.pri
+++ sstd_library/_sstd_library_memory.pri
@@ -4,14 +4,19 @@ win32-g++*{#win32-g++*
     DEFINES *= _0_SSTD_HAS_JE_MALLLOC
     LIBS += -L$$PWD/memory/libs -ljemalloc_win64_mingw_730
 
-} else {#win32-g++*
+} else {#!win32-g++*
 
 win32-msvc*{#win32-msvc*
 
     DEFINES *= _0_SSTD_HAS_JE_MALLLOC
     include($$PWD/memory/libs/jemalloc_msvc_64/jemalloc_msvc_64.pri)
 
-}#win32-msvc*
+} else { #!win32-msvc*
+
+    DEFINES *= _0_SSTD_HAS_TC_MALLOC
+    LIBS    += -ltcmalloc_minimal
+
+} #win32-msvc*
 
 }#win32-g++*
 
--- sstd_library/memory/sstd_memory.cpp
+++ sstd_library/memory/sstd_memory.cpp
@@ -8,34 +8,55 @@ extern void *je_aligned_alloc(size_t alignment, size_t size) ;
 }
 #endif
 
+#ifdef _0_SSTD_HAS_TC_MALLOC
+#include <gperftools/tcmalloc.h>
+#endif
+
 namespace  {
     inline void * auto_malloc(std::size_t n){
+#ifdef _0_SSTD_HAS_TC_MALLOC
+        return tc_malloc(n);
+#else
 #ifdef _0_SSTD_HAS_JE_MALLLOC
     return je_malloc(n);
 #else
     return ::operator new(n);
 #endif
+#endif
     }
     inline void auto_free(void *ptr){
-#ifdef _0_SSTD_HAS_JE_MALLLOC
+#ifdef _0_SSTD_HAS_TC_MALLOC
+        return tc_free(ptr);
+#else
+#if defined( _0_SSTD_HAS_JE_MALLLOC )
         return je_free(ptr);
 #else
         ::operator delete(ptr);
 #endif
+#endif
     }
     inline void * auto_malloc(std::size_t c, std::align_val_t a){
+#ifdef _0_SSTD_HAS_TC_MALLOC
+        return tc_memalign( static_cast<std::size_t>(a) , c );
+#else
 #ifdef _0_SSTD_HAS_JE_MALLLOC
     return je_aligned_alloc(static_cast<size_t>(a),c);
 #else
         return ::operator new(c, a);
 #endif
+#endif
     }
     inline void auto_free(void* ptr, std::align_val_t al){
+#ifdef _0_SSTD_HAS_TC_MALLOC
+        return tc_free(ptr);
+        (void)al;
+#else
 #ifdef _0_SSTD_HAS_JE_MALLLOC
         return je_free(ptr);
         (void)(al);
 #else
         return ::operator delete(ptr, al);
+#endif
 #endif
     }
 }/*namespace*/
