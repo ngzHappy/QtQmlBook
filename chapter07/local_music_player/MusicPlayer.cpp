99f4dd60cc4bd79f8d9a82e0c226a24ef66a0d45
ngzHappy <zhaixueqiang@hotmail.com>
2018-12-25 12:51:56 +0800

    +Test Code

--- chapter07/local_music_player/MusicPlayer.cpp
+++ chapter07/local_music_player/MusicPlayer.cpp
@@ -1,4 +1,82 @@
 ﻿#include "MusicPlayer.hpp"
+#include <QtMultimedia/QtMultimedia>
+
+namespace this_file {
+
+    /*构造QAudioFormat*/
+    inline QAudioFormat getAudioFormat(int rate = 44100) {
+        /****************************************************************/
+        /*统一将音频转为双声道，signed 16 整型，但采样频率不变*/
+        static const auto varAns = []() {
+            QAudioFormat varFormat;
+            varFormat.setSampleRate(44100);
+            varFormat.setChannelCount(2);
+            varFormat.setSampleSize(16);
+            varFormat.setCodec(QStringLiteral("audio/pcm"));
+#if Q_BYTE_ORDER == Q_BIG_ENDIAN
+            varFormat.setByteOrder(QAudioFormat::BigEndian);
+#else
+            varFormat.setByteOrder(QAudioFormat::LittleEndian);
+#endif
+            varFormat.setSampleType(QAudioFormat::SignedInt);
+            return varFormat;
+        }();
+        /****************************************************************/
+        auto var = varAns;
+        var.setSampleRate(rate);
+        return var;
+    }
+
+    /*构造QAudioDeviceInfo*/
+    inline QAudioDeviceInfo getAudioDeveceInfo(int rate = 44100) {
+
+        /*采用默认输出设备*/
+        QAudioDeviceInfo info(QAudioDeviceInfo::defaultOutputDevice());
+
+        if (!info.isFormatSupported(getAudioFormat(rate))) {
+            qWarning() <<
+                "Raw audio format not supported by backend, cannot play audio.";
+            /* std::exit(-999); */
+        }
+
+        return info;
+    }
+
+    /*音频输出设备*/
+    class AudioPlayer :
+        public QAudioOutput,
+        SSTD_BEGIN_DEFINE_VIRTUAL_CLASS(AudioPlayer) {
+    public:
+        AudioPlayer(int rate) :
+            QAudioOutput(
+                getAudioDeveceInfo(rate),
+                getAudioFormat(rate)) {
+        }
+    private:
+        SSTD_END_DEFINE_VIRTUAL_CLASS(AudioPlayer);
+    };
+
+    class AudioStream :
+        public QIODevice,
+        SSTD_BEGIN_DEFINE_VIRTUAL_CLASS(AudioStream){
+        sstd::intrusive_ptr<MusicReader > mmmReader;
+    public:
+        inline void construct(sstd::intrusive_ptr<MusicReader > arg) {
+            mmmReader = std::move(arg);
+            this->open(QIODevice::ReadOnly);
+        }
+        inline qint64 readData(char *data, qint64 maxSize) override {
+            return 0;
+        }
+        inline qint64 bytesAvailable() const override {
+            return 0;
+        }
+    private:
+        SSTD_END_DEFINE_VIRTUAL_CLASS(AudioStream);
+    };
+
+}/*this_file*/
+
 
 class _MusicPlayerPrivate {
     MusicPlayer * const mmmParent;
