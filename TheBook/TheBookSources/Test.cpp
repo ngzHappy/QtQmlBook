d0f75c5ac9d9d21703f0dd73fa41bf45651636f4
ngzHappy <zhaixueqiang@hotmail.com>
2019-01-02 12:25:56 +0800

    +little change

--- TheBook/TheBookSources/Test.cpp
+++ TheBook/TheBookSources/Test.cpp
@@ -3,17 +3,25 @@
 #include "the_book_global.hpp"
 #include "TexBuilder.hpp"
 
-static inline void test_00(){
-
-        auto varBuilder = std::make_shared<TexBuilder>();
-        varBuilder->setInputFileName(getOutPutFileFullPath(qsl("test/test_0000.txt")));
-        varBuilder->setOutputFileName(getOutPutFileFullPath(qsl("test/test_0000.tex")));
-        assert(varBuilder->convert());
+static inline void test_0000() {
+    auto varBuilder = std::make_shared<TexBuilder>();
+    varBuilder->setInputFileName(getOutPutFileFullPath(qsl("test/test_0000.txt")));
+    varBuilder->setOutputFileName(getOutPutFileFullPath(qsl("test/test_0000.tex")));
+    assert(varBuilder->convert());
 
 }
 
-extern void test_this(){
-    test_00();
+static inline void test_0100() {
+    auto varBuilder = std::make_shared<TexBuilder>();
+    varBuilder->setInputFileName(getOutPutFileFullPath(qsl("test/test_0100.txt")));
+    varBuilder->setOutputFileName(getOutPutFileFullPath(qsl("test/test_0100.tex")));
+    assert(varBuilder->convert());
+
+}
+
+extern void test_this() {
+    test_0000();
+    test_0100();
 }
 
 
