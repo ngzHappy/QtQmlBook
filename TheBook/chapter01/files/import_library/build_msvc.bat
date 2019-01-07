0f26ef930e02e0025ca9615d16f1c2afd61f27cc
ngzHappy <zhaixueqiang@hotmail.com>
2019-01-07 15:22:54 +0800

    +import library

---
+++ TheBook/chapter01/files/import_library/build_msvc.bat
@@ -0,0 +1,16 @@
+call "C:/Qt/Qt5.12.0/5.12.0/msvc2017_64/bin/qtenv2.bat"
+cd /D "E:/QtQmlBookMsvc"
+qmake -r -tp vc "E:/QtQmlBook/TheBook/chapter01/files/advance_use_qmake/import_library.pro"
+cmd
+
+
+
+
+
+
+
+
+
+
+
+
