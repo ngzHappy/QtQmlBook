69472c2cd244c3e31ad1fc1eaceb87e9390202a9
ngzHappy <zhaixueqiang@hotmail.com>
2019-01-12 19:40:33 +0800

    +little change

--- chapter07/local_video_player/VideoReader.hpp
+++ chapter07/local_video_player/VideoReader.hpp
@@ -1,7 +1,16 @@
 ﻿#pragma once
 
-class VideoReader{
+#include "AudioFrame.hpp"
+#include "AudioNumber.hpp"
+#include "FFMPEGOpenCloseThread.hpp"
 
+class VideoReader :
+    public virtual sstd_virtual_basic,
+    SSTD_BEGIN_DEFINE_VIRTUAL_CLASS_OVERRIDE(VideoReader)  {
+public:
+
+private:
+    SSTD_END_DEFINE_VIRTUAL_CLASS(VideoReader);
 };
 
 
