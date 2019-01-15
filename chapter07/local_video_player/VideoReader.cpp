2b2da4c77a2ebc077aa0df13814eba1680c1c0d1
ngzHappy <zhaixueqiang@hotmail.com>
2019-01-15 20:10:23 +0800

    +little change

--- chapter07/local_video_player/VideoReader.cpp
+++ chapter07/local_video_player/VideoReader.cpp
@@ -1,24 +1,18 @@
 ﻿#include "VideoReader.hpp"
 
+class _VideoReaderPirvate {
+public:
+    VideoReader* const super;
+    _VideoReaderPirvate (VideoReader* s) : super (s) {
+    }
+};
 
+VideoReader::VideoReader () :
+    thisp (sstd_new<_VideoReaderPirvate> (this)) {
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+VideoReader::~VideoReader () {
+}
 
 
 
--- chapter07/local_video_player/VideoReader.hpp
+++ chapter07/local_video_player/VideoReader.hpp
@@ -4,11 +4,15 @@
 #include "AudioNumber.hpp"
 #include "FFMPEGOpenCloseThread.hpp"
 
+class _VideoReaderPirvate;
 class VideoReader :
     public virtual sstd_intrusive_ptr_basic,
     SSTD_BEGIN_DEFINE_VIRTUAL_CLASS_OVERRIDE(VideoReader)  {
 public:
-
+    VideoReader();
+    ~VideoReader();
+private:
+    sstd$(thisp,const _VideoReaderPirvate *);
 private:
     SSTD_END_DEFINE_VIRTUAL_CLASS(VideoReader);
 };
