cea1a03b901d6fb03932520e3fa46a1fa636f616
ngzHappy <zhaixueqiang@hotmail.com>
2019-01-17 18:59:22 +0800

    +；

---
+++ sstd_tools/mingw_boost_dynamic/main.cpp
@@ -0,0 +1,51 @@
+﻿
+#if __has_include(<filesystem>)
+#include <filesystem>
+namespace fs = std::filesystem;
+#else
+#include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
+#endif
+
+#include <iostream>
+#include <fstream>
+
+#include <string>
+using namespace std::string_literals;
+
+int main(int ,char ** argv){
+
+    fs::path varRootFileName{ argv[0] };
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
