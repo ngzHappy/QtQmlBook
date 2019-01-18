9835c7b922a3b53e380813c011d5b56e6b24bd1c
ngzHappy <zhaixueqiang@hotmail.com>
2019-01-18 10:33:08 +0800

    +little change

--- sstd_library/container/sstd_container_utility.hpp
+++ sstd_library/container/sstd_container_utility.hpp
@@ -1,5 +1,20 @@
 ﻿#pragma once
 
+#include <utility>
+#include <iterator>
+#include <type_traits>
+
+namespace sstd::_12_private{
+     template<typename T,typename U,typename = void
+    > class CanEqual : public std::false_type {
+    };
+    template<typename T,typename U
+    > class CanEqual<T,U,std::void_t<
+        decltype(std::declval<T&&>() == std::declval<U&&>())>
+    > : public std::true_type {
+    };   
+}/*sstd::_12_private*/
+
 namespace sstd{
 
 /*move list frist to last ...*/
@@ -25,14 +40,24 @@ namespace sstd{
 /*move list element to last ...*/
 template<typename T,typename I>
 inline void move_list_element_to_back(T & arg,const I & pos) {
-    if(arg.cend() == pos) {
+    using ele_pos_type = decltype(arg.cbegin());
+    constexpr bool const canEqual =
+        _12_private::CanEqual<ele_pos_type,I>::value;
+    if constexpr(canEqual) {
+        if(arg.cend() == pos) {
+            return;
+        }
+        if(pos == (--arg.cend())) {
+            return;
+        }
+        arg.splice(arg.end(),arg,pos);
         return;
+    } else {
+        if(arg.crend() == pos) {
+            return;
+        }
+        return move_list_element_to_back(arg,--pos.base());
     }
-    if(pos == (--arg.cend())) {
-        return;
-    }
-    arg.splice(arg.end(),arg,pos);
-    return;
 }
 
 }/*sstd*/
@@ -42,14 +67,24 @@ namespace sstd{
 /*move list element to front ...*/
 template<typename T,typename I>
 inline void move_list_element_to_front(T & arg,const I & pos) {
-    if(arg.cbegin() == pos) {
+    using ele_pos_type = decltype(arg.cbegin());
+    constexpr bool const canEqual =
+        _12_private::CanEqual<ele_pos_type,I>::value;
+    if constexpr(canEqual) {
+        if(arg.cbegin() == pos) {
+            return;
+        }
+        if(arg.cend() == pos) {
+            return;
+        }
+        arg.splice(arg.begin(),arg,pos);
         return;
+    } else {
+        if(arg.crend() == pos) {
+            return;
+        }
+        return move_list_element_to_front(arg,--pos.base());
     }
-    if(arg.cend() == pos) {
-        return;
-    }
-    arg.splice(arg.begin(),arg,pos);
-    return;
 }
 
 }/*sstd*/
