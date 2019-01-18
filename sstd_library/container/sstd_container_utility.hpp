06d543c368de720286f7c88d305fc845f833c7e9
ngzHappy <zhaixueqiang@hotmail.com>
2019-01-18 09:33:07 +0800

    +little change

---
+++ sstd_library/container/sstd_container_utility.hpp
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+namespace sstd{
+
+/*move list frist to last ...*/
+template<typename T>
+inline void move_list_front_to_back(T & arg) {
+    const auto varListSize = arg.size();
+    if(varListSize > 2) {
+        arg.splice(arg.end(),arg,arg.cbegin());
+        return;
+    }
+    if(varListSize < 2) {
+        /*if empty or just one element do nothing*/
+        return;
+    }
+    arg.reverse();
+    return;
+}
+
+}/*sstd*/
+
+
+
+
+
+
+
+
+
+
+
+
+
