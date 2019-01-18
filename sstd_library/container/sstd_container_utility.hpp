1b93d31b18243445bc0852d8acbfffb9e48946ff
ngzHappy <zhaixueqiang@hotmail.com>
2019-01-18 10:38:19 +0800

    +

--- sstd_library/container/sstd_container_utility.hpp
+++ sstd_library/container/sstd_container_utility.hpp
@@ -36,6 +36,26 @@ inline void move_list_front_to_back(T & arg) {
 }/*sstd*/
 
 namespace sstd{
+
+/*move list last to first ...*/
+template<typename T>
+inline void move_list_back_to_front(T & arg) {
+    const auto varListSize = arg.size();
+    if(varListSize > 2) {
+        arg.splice(arg.begin(),arg,--arg.cend());
+        return;
+    }
+    if(varListSize < 2) {
+        /*if empty or just one element do nothing*/
+        return;
+    }
+    arg.reverse();
+    return;
+}
+
+}/*sstd*/
+
+namespace sstd{
     
 /*move list element to last ...*/
 template<typename T,typename I>
