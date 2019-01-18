bc829cfd94c6ebf95568fcbcb1b55d5c255d49ab
ngzHappy <zhaixueqiang@hotmail.com>
2019-01-18 09:45:08 +0800

    +little change

--- sstd_library/container/sstd_container_utility.hpp
+++ sstd_library/container/sstd_container_utility.hpp
@@ -20,11 +20,39 @@ inline void move_list_front_to_back(T & arg) {
 
 }/*sstd*/
 
+namespace sstd{
+    
+/*move list element to last ...*/
+template<typename T,typename I>
+inline void move_list_element_to_back(T & arg,const I & pos) {
+    if(arg.cend() == pos) {
+        return;
+    }
+    if(pos == (--arg.cend())) {
+        return;
+    }
+    arg.splice(arg.end(),arg,pos);
+    return;
+}
 
+}/*sstd*/
 
+namespace sstd{
 
+/*move list element to front ...*/
+template<typename T,typename I>
+inline void move_list_element_to_front(T & arg,const I & pos) {
+    if(arg.cbegin() == pos) {
+        return;
+    }
+    if(arg.cend() == pos) {
+        return;
+    }
+    arg.splice(arg.begin(),arg,pos);
+    return;
+}
 
-
+}/*sstd*/
 
 
 
