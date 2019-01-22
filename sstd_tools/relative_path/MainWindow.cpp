616be9609c89c461ad48e67779b54f294839e3d7
ngzHappy <zhaixueqiang@hotmail.com>
2019-01-23 06:50:52 +0800

    +little change

--- sstd_tools/relative_path/MainWindow.cpp
+++ sstd_tools/relative_path/MainWindow.cpp
@@ -4,71 +4,71 @@
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow){
+    ui(new Ui::MainWindow) {
     ui->setupUi(this);
-    connect( ui->sbA,&QPushButton::clicked ,
-             this,&MainWindow::_on_sbA_clicked );
-    connect( ui->sbB,&QPushButton::clicked ,
-             this,&MainWindow::_on_sbB_clicked );
-    connect( ui->rb,&QPushButton::clicked ,
-             this,&MainWindow::_on_rb_clicked );
+    connect(ui->sbA,&QPushButton::clicked,
+        this,&MainWindow::_on_sbA_clicked);
+    connect(ui->sbB,&QPushButton::clicked,
+        this,&MainWindow::_on_sbB_clicked);
+    connect(ui->rb,&QPushButton::clicked,
+        this,&MainWindow::_on_rb_clicked);
 
     QDir varDir{ CURRENT_DEBUG_PATH };
-    ui->leA->setText( QDir(
-                varDir.absoluteFilePath((QStringLiteral("../../TheBook"))))
-                      .canonicalPath());
+    ui->leA->setText(QDir(
+        varDir.absoluteFilePath((QStringLiteral("../../TheBook"))))
+        .canonicalPath());
 
-    ui->leB->setText( ui->leA->text()
-                      +QStringLiteral("/main_index.txt") );
+    ui->leB->setText(ui->leA->text()
+        + QStringLiteral("/main_index.txt"));
 
 }
 
-MainWindow::~MainWindow(){
+MainWindow::~MainWindow() {
     delete ui;
 }
 
-namespace  {
+namespace {
 
-    QString selectFile(QLineEdit * arg){
+    QString selectFile(QLineEdit * arg) {
         const QDir varDir{ arg->text() };
         return QFileDialog::getOpenFileName(
-                    nullptr,
-                    QObject::trUtf8(u8R"(选择文件：)"),
-                    varDir.dirName());
+            nullptr,
+            QObject::trUtf8(u8R"(选择文件：)"),
+            varDir.dirName());
     }
 
-    QString selectDir(QLineEdit * arg){
+    QString selectDir(QLineEdit * arg) {
         return QFileDialog::getExistingDirectory(
-                    nullptr,
-                    QObject::trUtf8(u8R"(选择文件夹：)"),
-                    arg->text());
+            nullptr,
+            QObject::trUtf8(u8R"(选择文件夹：)"),
+            arg->text());
     }
 
 }/*namespace*/
 
-void MainWindow::_on_sbA_clicked(){
-    auto varAns = selectDir( ui->leA );
-    if(varAns.isEmpty()){
+void MainWindow::_on_sbA_clicked() {
+    auto varAns = selectDir(ui->leA);
+    if(varAns.isEmpty()) {
         return;
     }
     ui->leA->setText(varAns);
 }
 
-void MainWindow::_on_sbB_clicked(){
-    auto varAns = selectFile( ui->leB );
-    if(varAns.isEmpty()){
+void MainWindow::_on_sbB_clicked() {
+    auto varAns = selectFile(ui->leB);
+    if(varAns.isEmpty()) {
         return;
     }
     ui->leB->setText(varAns);
 }
 
-void MainWindow::_on_rb_clicked(){
+void MainWindow::_on_rb_clicked() {
     auto varRootPath = ui->leA->text();
     auto varFilePath = ui->leB->text();
 
     QDir varRoot{ varRootPath };
     auto varAns =
-            varRoot.relativeFilePath(varFilePath);
+        varRoot.relativeFilePath(varFilePath);
 
     ui->la->setText(varAns);
 
