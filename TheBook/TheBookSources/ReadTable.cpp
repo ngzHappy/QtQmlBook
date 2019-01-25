c7fe45e8b25c8ac9077bb502979bd1b4e0f7865f
ngzHappy <zhaixueqiang@hotmail.com>
2019-01-25 09:47:52 +0800

    +little change

--- TheBook/TheBookSources/ReadTable.cpp
+++ TheBook/TheBookSources/ReadTable.cpp
@@ -1,4 +1,4 @@
-﻿
+﻿#include "ReadTable.hpp"
 
 
 
--- TheBook/TheBookSources/ReadTable.hpp
+++ TheBook/TheBookSources/ReadTable.hpp
@@ -1,6 +1,18 @@
-﻿
+﻿#pragma once
 
+#include <memory>
+#include "the_book_global.hpp"
 
+class ReadTablePrivate;
+class ReadTable{
+public:
+    ReadTable();
+public:
+    const QString & getTableDirName();
+    void setTableDirName(const QString &);
+private:
+    ReadTablePrivate * const thisp;
+};
 
 
 
