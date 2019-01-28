7f9710fddb096f97dd103064444b324336a36c1e
ngzHappy <zhaixueqiang@hotmail.com>
2019-01-28 10:08:42 +0800

    +更改图片边缘

--- TheBook/TheBookSources/ImageConvert.cpp
+++ TheBook/TheBookSources/ImageConvert.cpp
@@ -18,6 +18,68 @@ bool ImageConvert::needConvert() const {
     return true;
 }
 
+void drawImageBorder(
+    QImage * arg1,
+    int width,
+    const QColor & color) {
+
+    auto varTmpImage =
+        arg1->convertToFormat(QImage::Format::Format_RGBA8888);
+    QImage * arg = &varTmpImage;
+
+    class Color {
+    public:
+        std::uint8_t r;
+        std::uint8_t g;
+        std::uint8_t b;
+        std::uint8_t a;
+    };
+
+    const Color varColor{
+        static_cast<std::uint8_t>(color.red() & 255) ,
+        static_cast<std::uint8_t>(color.green() & 255) ,
+        static_cast<std::uint8_t>(color.blue() & 255) ,
+        static_cast<std::uint8_t>(color.alpha() & 255) };
+
+    const auto varSkip = arg->bytesPerLine();
+    auto varBeginOfTheImage = arg->bits();
+    auto varCurrentLine =
+        reinterpret_cast<Color *>(varBeginOfTheImage);
+    auto varCurrentLineBegin = varBeginOfTheImage;
+
+    const auto varImageHeight = arg->height();
+    const auto varImageWidth = arg->width();
+    const auto varLeftLimit = width;
+    const auto varRightLimit = varImageWidth > width ? (varImageWidth - width) : 0;
+    const auto varTopLimit = width;
+    const auto varBottomLimit = varImageHeight > width ? (varImageHeight - width) : 0;
+
+    for (
+        int varLineCount = 0;
+        varLineCount < varImageHeight;
+        ++varLineCount,
+        varCurrentLineBegin += varSkip,
+        varCurrentLine = reinterpret_cast<Color *>(varCurrentLineBegin)) {
+
+        assert(varCurrentLineBegin == arg->constScanLine(varLineCount));
+
+        for (int varColumnCount = 0;
+            varColumnCount < varImageWidth;
+            ++varColumnCount) {
+            if ((varColumnCount <= varLeftLimit) || (varColumnCount >= varRightLimit) ||
+                (varLineCount <= varTopLimit) || (varLineCount >= varBottomLimit)) {
+                auto varPos =
+                    varCurrentLine + varColumnCount;
+                *varPos = varColor;
+            }
+        }
+
+    }
+
+    *arg1 = *arg;
+
+}
+
 bool ImageConvert::convert() {
 
     QString varCommand;
@@ -32,6 +94,27 @@ bool ImageConvert::convert() {
         InputStream varReadStream{ &varReadFile };
         varCommand =
             varReadStream.readAll().trimmed();
+    }
+
+    {
+        QImage varImage{
+            input_image_full_path
+        };
+        if (varImage.isNull()) {
+            return false;
+        }
+        {
+            output_image_relative_path =
+                qsl("the_book_image/") +
+                image_index +
+                qsl(".png");
+            output_image_full_path =
+                getOutPutFileFullPath(
+                    output_image_relative_path);
+            input_image_full_path = output_image_full_path;
+        }
+        drawImageBorder(&varImage, 1, QColor(255, 255, 255, 255));
+        varImage.save(output_image_full_path);
     }
 
     {
@@ -57,7 +140,6 @@ bool ImageConvert::convert() {
         varProcess.terminate();
         return false;
     }
-   
 
 }
 
