f1e3d15cfeee5310d3ef0b638faa6969d7ef1cd9
ngzHappy <zhaixueqiang@hotmail.com>
2019-01-29 12:16:04 +0800

    +little change

--- sstd_tools/create_dll_link/MainWindow.cpp
+++ sstd_tools/create_dll_link/MainWindow.cpp
@@ -3,6 +3,8 @@
 #include <list>
 #include <iostream>
 #include <exception>
+#include <string_view>
+using namespace std::string_view_literals;
 
 #if __has_include(<filesystem>)
 #include <filesystem>
@@ -48,9 +50,27 @@ namespace this_file {
             source_file(std::move(arg)) {
         }
     };
+
+    static inline bool needCreateLink(const QString & arg) {
+        const static QRegularExpression varExp{
+            QStringLiteral(R"!((?:dll)|(?:conf))!"),
+            QRegularExpression::CaseInsensitiveOption };
+        return varExp.match(arg).hasMatch();
+    }
+
 }/*namespace this_file*/
 
 void MainWindow::on_runButton_clicked() {
+
+    std::cout << u8R"(
+change qt.conf lick this ...
+[Paths]
+Documentation=D:/Qt/Qt5.12.0/Docs/Qt-5.12.0
+Examples=D:/Qt/Qt5.12.0/Examples/Qt-5.12.0
+Prefix=D:/Qt/Qt5.12.0/5.12.0/msvc2017_64
+
+)"sv;
+
     const auto varSourceText =
         ui->sourceText->text();
     const auto varTargetText =
@@ -60,9 +80,12 @@ void MainWindow::on_runButton_clicked() {
     {
         QDirIterator varDir{ varSourceText };
         while (varDir.hasNext()) {
-            const auto varFileName = varDir.next();
+            varDir.next();
             auto varFileInfo = varDir.fileInfo();
             if (!varFileInfo.isFile()) {
+                continue;
+            }
+            if (!this_file::needCreateLink(varFileInfo.suffix())) {
                 continue;
             }
             varDuty.emplace_back(std::move(varFileInfo));
@@ -84,7 +107,7 @@ void MainWindow::on_runButton_clicked() {
         try {
             fs::create_symlink(varSourceSTD, varTargetSTD);
         } catch (const std::exception & arg) {
-            std::cout<< arg.what() <<std::endl ;
+            std::cout << arg.what() << std::endl;
             continue;
         }
     }
