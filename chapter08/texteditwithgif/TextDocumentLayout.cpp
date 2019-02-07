b5ec7d664cdca82a2c0cdbe714ed165e8c90a9ab
ngzHappy <zhaixueqiang@hotmail.com>
2019-02-07 17:38:22 +0800

    +

--- chapter08/texteditwithgif/TextDocumentLayout.cpp
+++ chapter08/texteditwithgif/TextDocumentLayout.cpp
@@ -5,14 +5,65 @@ TextDocumentLayout::TextDocumentLayout(QTextDocument * arg) :
 
 }
 
-void TextDocumentLayout::documentChanged(
-    int position,
-    int charsRemoved,
-    int charsAdded) {
+void TextDocumentLayout::documentChanged(int position, int charsRemoved, int charsAdded) {
+    {/*更新索引*/
+        decltype(mmmQmlItems) varNewQmlItems;
+
+        const auto varDx = charsAdded - charsRemoved;
+        for (const auto & varI : mmmQmlItems) {
+            auto varOldPos = varI.first;
+            if (varOldPos < position) {/*索引没有变化*/
+                varNewQmlItems.emplace(varI.first, std::move(varI.second));
+            } else if ((varOldPos - position) < charsRemoved) {/*删除了此元素*/
+                continue;
+            } else if (varDx == 0) {/*索引没有变化*/
+                varNewQmlItems.emplace(
+                    varI.first,
+                    std::move(varI.second));
+            } else {/*>=position索引变化*/
+                varNewQmlItems.emplace(
+                    varDx + varI.first,
+                    std::move(varI.second));
+            }
+        }
+        mmmQmlItems = std::move(varNewQmlItems);
+    }
+
+    if (charsAdded < 1) {
+        return;
+    }
+
+    /*插入新的QMl*/
+    for (int i = 0; i < charsAdded; ++i) {
+        const auto varCurrentPos = position + i;
+        auto varCharFormat = this->format(varCurrentPos);
+        if (!varCharFormat.isImageFormat()) {
+            continue;
+        }
+        const auto varImageFormat = varCharFormat.toImageFormat();
+        const auto varImageName = varImageFormat.name();
+        if (varImageName.endsWith(QStringLiteral(".qml"), Qt::CaseInsensitive)) {
+            mmmQmlItems.insert({ varCurrentPos,
+                sstd_make_shared<TextQmlWrappedItem>(
+                    varImageName,
+                    varImageFormat.width(),
+                    varImageFormat.height()) });
+        }
+    }
+
     Super::documentChanged(position, charsRemoved, charsAdded);
 }
 
+TextQmlWrappedItem::TextQmlWrappedItem(
+    const QString & arg, int w, int h) : mmmQmlPath(arg),
+    mmmImageWidth(w),
+    mmmImageHeight(h) {
 
+}
+
+TextQmlWrappedItem::~TextQmlWrappedItem() {
+
+}
 
 
 
--- chapter08/texteditwithgif/TextDocumentLayout.hpp
+++ chapter08/texteditwithgif/TextDocumentLayout.hpp
@@ -3,6 +3,17 @@
 #include <sstd_qt_and_qml_library.hpp>
 #include <QtGui/private/qtextdocumentlayout_p.h>
 
+class TextQmlWrappedItem {
+    const QString mmmQmlPath;
+    const int mmmImageWidth;
+    const int mmmImageHeight;
+public:
+    TextQmlWrappedItem(const QString &,int,int);
+    ~TextQmlWrappedItem();
+private:
+    SSTD_DEFINE_STATIC_CLASS(TextQmlWrappedItem);
+};
+
 class TextDocumentLayout :
     public QTextDocumentLayout,
     SSTD_BEGIN_DEFINE_VIRTUAL_CLASS(TextDocumentLayout) {
@@ -14,6 +25,11 @@ public:
     TextDocumentLayout(QTextDocument *);
 protected:
     void documentChanged(int position, int charsRemoved, int charsAdded) override;
+private:
+    sstd::map< 
+        int, 
+        std::shared_ptr< TextQmlWrappedItem  > 
+    > mmmQmlItems;
 private:
     SSTD_END_DEFINE_VIRTUAL_CLASS(TextDocumentLayout);
 };
