db57373941728f083a1110301ec45ff596c07777
ngzHappy <zhaixueqiang@hotmail.com>
2019-02-10 14:55:11 +0800

    +

--- chapter08/chatview/ChatDataItem.cpp
+++ chapter08/chatview/ChatDataItem.cpp
@@ -5,7 +5,13 @@
 
 
 
-
+static inline void register_this() {
+    qmlRegisterType<ChatDataItem>(
+        "sstd.chat.help",
+        1, 0,
+        "ChatDataItem");
+}
+Q_COREAPP_STARTUP_FUNCTION(register_this)
 
 
 
--- chapter08/chatview/ChatHelper.cpp
+++ chapter08/chatview/ChatHelper.cpp
@@ -17,7 +17,13 @@
 
 
 
-
+static inline void register_this() {
+    qmlRegisterType<ChatHelper>(
+        "sstd.chat.help",
+        1, 0,
+        "ChatHelper");
+}
+Q_COREAPP_STARTUP_FUNCTION(register_this)
 
 
 
