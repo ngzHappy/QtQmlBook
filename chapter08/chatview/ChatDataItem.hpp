ac238a6f8959ff117bcf9310145feb55abb3aae7
ngzHappy <zhaixueqiang@hotmail.com>
2019-02-10 14:51:09 +0800

    +

--- chapter08/chatview/ChatDataItem.hpp
+++ chapter08/chatview/ChatDataItem.hpp
@@ -2,8 +2,12 @@
 
 #include <sstd_qt_and_qml_library.hpp>
 
-class ChatDataItem : public QObject {
+class ChatDataItem :
+        public QObject ,
+        SSTD_BEGIN_DEFINE_VIRTUAL_CLASS(ChatDataItem) {
     Q_OBJECT
+    private:
+    SSTD_END_DEFINE_VIRTUAL_CLASS(ChatDataItem);
 };
 
 
--- chapter08/chatview/ChatHelper.hpp
+++ chapter08/chatview/ChatHelper.hpp
@@ -2,8 +2,11 @@
 
 #include <sstd_qt_and_qml_library.hpp>
 
-class ChatHelper : public QObject {
+class ChatHelper : public QQuickItem ,
+        SSTD_BEGIN_DEFINE_VIRTUAL_CLASS(ChatHelper) {
     Q_OBJECT
+    private:
+    SSTD_END_DEFINE_VIRTUAL_CLASS(ChatHelper);
 };
 
 
