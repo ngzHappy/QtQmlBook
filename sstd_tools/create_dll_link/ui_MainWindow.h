b73ef6adff6e3b694f12b044ff9704dc54f081da
ngzHappy <zhaixueqiang@hotmail.com>
2019-02-17 15:01:16 +0800

    +

--- sstd_tools/create_dll_link/ui_MainWindow.h
+++ sstd_tools/create_dll_link/ui_MainWindow.h
@@ -1,7 +1,7 @@
-﻿/********************************************************************************
+/********************************************************************************
 ** Form generated from reading UI file 'MainWindow.ui'
 **
-** Created by: Qt User Interface Compiler version 5.12.0
+** Created by: Qt User Interface Compiler version 5.12.1
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
